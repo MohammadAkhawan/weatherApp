fec052b20183f26bf8b4608fd4d8b45b14a3bd78
mammad <113013285+MohammadAkhawan@users.noreply.github.com>
2024-03-22 23:30:00 -0400

    finish basic style and visual report

--- script.js
+++ script.js
@@ -1,17 +1,74 @@
 const getApi = async (nameOfCity) => {
-    const response = await fetch(
-        `https://api.weatherapi.com/v1/current.json?key=3d04a3019ac744f0bde173915242203&q=${nameOfCity}`
-    );
-    const json = await response.json();
-    console.log(json);
+    try {
+        const response = await fetch(
+            `https://api.weatherapi.com/v1/current.json?key=3d04a3019ac744f0bde173915242203&q=${nameOfCity}`
+        );
+        if (!response.ok) {
+            throw new Error("Network response was not ok.");
+        }
+        const json = await response.json();
+        return json;
+    } catch (error) {
+        console.error("Error fetching data:", error);
+        return null; // or handle the error in an appropriate way
+    }
 };
 
+const getImportantInfo = (cityObj) => {
+    const { temp_c, feelslike_c, wind_kph, uv } = cityObj.current;
+    const condition = cityObj.current.condition.text;
+    return {
+        temp_c,
+        feelslike_c,
+        wind_kph,
+        uv,
+        condition,
+    };
+};
 
+const resetFields = () => {
+    conditionElement.textContent = "";
+    tempElement.textContent = "Temp in C: ";
+    feelslikeElement.textContent = "Feels Like in C: ";
+    windElement.textContent = "wind speed: ";
+    uvElement.textContent = "uv: ";
+};
 
+const showWeatherInfo = (obj) => {
+    resetFields();
+
+    conditionElement.textContent += obj.condition;
+    containerElement.appendChild(conditionElement);
+
+    tempElement.textContent += obj.temp_c;
+    containerElement.appendChild(tempElement);
+
+    feelslikeElement.textContent += obj.feelslike_c;
+    containerElement.appendChild(feelslikeElement);
+
+    windElement.textContent += obj.wind_kph;
+    containerElement.appendChild(windElement);
+
+    uvElement.textContent += obj.uv;
+    containerElement.appendChild(uvElement);
+};
+
+const containerElement = document.querySelector(".container");
 const searchBtn = document.querySelector("#search");
 const userInput = document.querySelector("input");
+const conditionElement = document.createElement("p");
+const tempElement = document.createElement("p");
+const feelslikeElement = document.createElement("p");
+const windElement = document.createElement("p");
+const uvElement = document.createElement("p");
 
-searchBtn.addEventListener("click", () => {
+searchBtn.addEventListener("click", async () => {
     const city = userInput.value;
-    getApi(city);
+    try {
+        const cityData = await getApi(city);
+        const cityInfoObj = getImportantInfo(cityData);
+        showWeatherInfo(cityInfoObj);
+    } catch (error) {
+        console.error("Error:", error);
+    }
 });
